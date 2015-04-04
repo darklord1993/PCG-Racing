35a35c705147337a01ffc04f2a2b2df58387a768
Chase Plante <Plante.8@buckeyemail.osu.edu>
2015-04-04 18:08:38 -0400

    Initial Tile Generation

---
+++ PCG-Racing/Assets/Generator.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PCGRacing
+{
+    public class Generator : MonoBehaviour
+    {
+        public float width;
+        public float height;
+
+        //Prefabs
+        public GameObject left;
+        public GameObject straight;
+        public GameObject right;
+
+        public string trackCode;
+        private List<Tile> track;
+        private Direction nextDir;
+        private int nextX;
+        private int nextY;
+
+        // Use this for initialization
+        void Start()
+        {
+            track = new List<Tile>();
+            nextX = 0;
+            nextY = 0;
+            nextDir = Direction.W;
+
+            for (int i = 0; i < trackCode.Length; i++)
+            {
+                track.Add(new Tile(nextX, nextY, nextDir));
+
+                if(trackCode[i] == 'r')
+                {
+                    track[i].type = TileType.Right;
+                    track[i].prefab = right;
+                    if (nextDir == Direction.N) { nextX += 1; nextDir = Direction.E; }
+                    else if (nextDir == Direction.S) { nextX -= 1; nextDir = Direction.W; }
+                    else if (nextDir == Direction.E) { nextY -= 1; nextDir = Direction.S; }
+                    else if (nextDir == Direction.W) { nextY += 1; nextDir = Direction.N; }
+                    
+                }
+                else if(trackCode[i] == 's')
+                {
+                    track[i].type = TileType.Straight;
+                    track[i].prefab = straight;
+                    if (nextDir == Direction.N) nextY += 1;
+                    else if (nextDir == Direction.S) nextY -= 1;
+                    else if (nextDir == Direction.E) nextX += 1;
+                    else if (nextDir == Direction.W) nextX -= 1;
+                    //Direction remains the same
+                }
+                else if (trackCode[i] == 'l')
+                {
+                    track[i].type = TileType.Left;
+                    track[i].prefab = left;
+                    if (nextDir == Direction.N) { nextX -= 1; nextDir = Direction.W; }
+                    else if (nextDir == Direction.S) { nextX += 1; nextDir = Direction.E; }
+                    else if (nextDir == Direction.E) { nextY += 1; nextDir = Direction.N; }
+                    else if (nextDir == Direction.W) { nextY -= 1; nextDir = Direction.S; }
+                }
+
+                track[i].Generate(width, height);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ PCG-Racing/Assets/Tile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PCGRacing
+{
+    public class Tile
+    {
+        public int x;
+        public int y;
+        public TileType type;
+        public Direction dir;
+        public GameObject prefab;
+
+        public Tile(int x, int y, Direction dir)
+        {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public void Generate(float width, float height)
+        {
+            var tile = GameObject.Instantiate(prefab, new Vector3(x * width, 0f, y * height), Quaternion.identity) as GameObject;
+
+            if (dir == Direction.W)
+            {
+                tile.transform.LookAt(new Vector3(x * width + 1, 0f, y * height));
+            }
+            else if (dir == Direction.S)
+            {
+                tile.transform.LookAt(new Vector3(x * width, 0f, y * height + 1));
+            }
+            else if (dir == Direction.N)
+            {
+                tile.transform.LookAt(new Vector3(x * width, 0f, y * height - 1));
+            }
+            else if (dir == Direction.E)
+            {
+                tile.transform.LookAt(new Vector3(x * width - 1, 0f, y * height));
+            }
+        }
+    }
+
+    public enum TileType
+    {
+        Left,
+        Right,
+        Straight
+    }
+
+    public enum Direction
+    {
+        N,
+        S,
+        E,
+        W
+    }
+}
