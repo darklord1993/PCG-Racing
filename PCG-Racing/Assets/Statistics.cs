14196c0498e559f0406bd9ca00c3da1005f74fc4
aathornburg <alan.thornb@gmail.com>
2015-05-03 13:54:29 -0400

    Added some statistics for the tracks generated last night.  Can hopefully use this on the paper.
    Statistics are in Assets/Outputs/Statistics.txt

---
+++ PCG-Racing/Assets/Statistics.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Statistics : MonoBehaviour {
+
+	string[] tracksWithErrors;
+	// Use this for initialization
+	void Start ()
+	{
+		tracksWithErrors = System.IO.File.ReadAllLines ("Assets/Outputs/TracksWithErrors.txt");
+
+		ConductAnalysis ();
+	}
+	
+	private void ConductAnalysis()
+	{
+		float totalError = 0, avgError = 0;
+		float highestError = 0, lowestError = -1;
+		string worstTrack = string.Empty, bestTrack = string.Empty;
+
+		for (int i = 0; i < tracksWithErrors.Length; i++)
+		{
+			string trackwithError = tracksWithErrors[i];
+			string[] trackInfo = trackwithError.Split(',');
+			float error = float.Parse(trackInfo[1]);
+
+			totalError += error;
+
+			if (highestError < error)
+			{
+				highestError = error;
+				worstTrack = trackInfo[0];
+			}
+			if ((lowestError < 0) || (lowestError > error))
+			{
+				lowestError = error;
+				bestTrack = trackInfo[0];
+			}
+		}
+
+		avgError = totalError / tracksWithErrors.Length;
+
+		Debug.Log ("Average Error: " + avgError);
+		Debug.Log ("Total Error: " + totalError);
+		Debug.Log ("Highest Error: " + highestError);
+		Debug.Log ("Worst Track: " + worstTrack);
+		Debug.Log ("Lowest Error: " + lowestError);
+		Debug.Log ("Best Track: " + bestTrack);
+	}
+}
