a3760f4f83dc0700011438771cef7833ce6dd6a3
aathornburg <alan.thornb@gmail.com>
2015-05-03 16:31:56 -0400

    Sorter as a work-in-progress.  Will continue working on it at the meeting.  Next step is to change generation to use the sorted tiles for generation that is quicker

---
+++ PCG-Racing/Assets/Sorter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sorter : MonoBehaviour {
+
+	private string[] metatiles;
+	private string currentLowest;
+
+	void Start ()
+	{
+		metatiles = System.IO.File.ReadAllLines ("Assets/Inputs/Metatiles/Turns.txt");
+		//metatiles = System.IO.File.ReadAllLines ("Assets/Inputs/Metatiles/Straights.txt");
+
+		Sort ();
+	}
+	
+	private void Sort()
+	{
+		List<string> unsortedTiles = new List<string> (metatiles);
+		List<string> sortedTiles = new List<string> ();
+
+		while (unsortedTiles.Count > 0)
+		{
+			float lowestSpeed = 1.0f;
+			string lowestSpeedLine = string.Empty;
+
+			for (int i = 0; i < unsortedTiles.Count; i++)
+			{
+				string trackWithMetrics = unsortedTiles[i];
+				string[] trackInfo = trackWithMetrics.Split(',');
+				float speedValue = float.Parse(trackInfo[1]);
+
+				if (lowestSpeed > speedValue)
+				{
+					lowestSpeedLine = trackWithMetrics;
+					lowestSpeed = speedValue;
+				}
+			}
+
+			unsortedTiles.Remove(lowestSpeedLine);
+			sortedTiles.Add(lowestSpeedLine);
+		}
+
+		//System.IO.File.WriteAllLines ("Assets/Outputs/Test.txt", sortedTiles.ToArray ());
+	}
+}
