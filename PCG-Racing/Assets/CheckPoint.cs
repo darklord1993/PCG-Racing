918ff85af053fec6c4c49d3d93751f792f1e3127
MillerKevinW <miller.5649@osu.edu>
2015-05-03 16:15:45 -0400

    Added Some Music
    Music and Checkpoint sounds now play

---
+++ PCG-Racing/Assets/CheckPoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour {
+
+	public AudioSource checkpointSound;
+	public bool isActive;
+	public int timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!isActive)
+			timer++;
+
+		if (timer >= 200) {
+			isActive = true;
+			timer = 0;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (isActive) 
+		{
+			checkpointSound.Play ();
+			isActive = false;
+		}
+	}
+}
